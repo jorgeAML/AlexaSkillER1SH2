11c2ee18243e3f5bfa451a2de6dd2a117ad933f9
jorgeAML <martjorge8@gmail.com>
2022-08-21 08:16:31 -0600

    interaction model 1

--- lambda/DBHistoria.js
+++ lambda/DBHistoria.js
@@ -1,5 +1,5 @@
 const dbhistorias = [
-    { historiaP0:"Ubico se había preparado una año atrás para la campaña electoral. Washington logra que las elecciones se hagan de inmediato, en febrero del mismo año, a fin de que Ubico sea el candidato con mayor oportunidad. Ganando las elecciones con más de 300 mil votos."},
+    { historiaP0:"Ubico se había preparado un año atrás para la campaña electoral. Washington logra que las elecciones se hagan de inmediato, en febrero del mismo año, a fin de que Ubico sea el candidato con mayor oportunidad. Ganando las elecciones con más de 300 mil votos."},
     { parte1: "ss"}
     ];
     
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ This is one of many of the projects called ERSH2 that is from my university the 
 
 This is version 1.0.0
 This brief description will change every time while we working on.
+
+To call the skill you might to use "ascenso ubico"
